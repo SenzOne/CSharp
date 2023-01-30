3b8433d17ef5e2707415df20e2f32e2963e384b0
Semen Sl <sem63sam@yandex.ru>
2023-01-30 14:39:49 +0400

    completed task 1

---
+++ homework_2/task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.Write("Entered a three-digit number: ");
+int input_number = 456; //Convert.ToInt32(Console.Read());
+
+System.Console.WriteLine((input_number / 10) % 10);
