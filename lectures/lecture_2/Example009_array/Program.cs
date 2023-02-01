3e2b4561a8de7a9a741b234637caa9e282a94b27
Semen Sl <sem63sam@yandex.ru>
2023-02-01 23:21:20 +0400

    rename folder letures, add lecture 2

---
+++ lectures/lecture_2/Example009_array/Program.cs
@@ -0,0 +1,18 @@
+﻿// тип данных[] имя = {значение1, значение2, ...}
+
+// int[] array = {1, 2, 3, 4, 5};
+// int[] array2 = new int[5]; // массив состоящий из 5 нулей
+// int[] array3 = new int[] {0, 0, 0, 0, 0};
+// int[] array3 = new int[5] {1, 0, 2, 0, 3};
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int res = arg1;
+    if (arg2 > res) res = arg2;
+    if (arg3 > res) res = arg3;
+    return res;
+}
+
+int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+array[0] = 12;
+System.Console.WriteLine(array[0]);
---
+++ lectures/lecture_2/Example010_metodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 3, 4, 345, 54, 23, 42 };
+int n = array.Length;
+int faind = 42;
+int index = 0;
+while (index < n)
+{
+    if (faind == array[index])
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    else
+        index++;
+}
