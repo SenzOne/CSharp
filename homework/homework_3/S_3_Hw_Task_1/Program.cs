ed62c7f2cd56243da13ef2952d7873105b8ce6ec
Semen Sl <sem63sam@yandex.ru>
2023-02-03 21:30:33 +0400

    add homework 3

---
+++ homework/homework_3/S_3_Hw_Task_1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Я решил немного усложнить задачу, и сделать так, чтобы программа обрабатывала
+// палиндромом любой длинны но так, как нельзя пользоваться срезами и метадами массивов
+// пришлошь испольховать большое количество циклов 
+
+// Функция преобразовывает аргумент в массив и возвращает его
+int[] GenerateArrFromNum(int number)
+{
+    int number_count = number;       // создаем копию числа, которое будем уменьшать
+    int conut = 0;                   // количество знаков в number
+    while (number_count > 0)
+    {
+        number_count /= 10;
+        conut++;
+    }
+
+    int[] arr = new int[conut];      // создаем массив длинны count, которое получили в прошлом цикле  
+    for (int i = number, j = 0; i > 0; i /= 10, j++)
+    {
+        arr[j] = i % 10;             // заполняем отдельными числами массив
+    }
+    return arr;
+}
+
+
+// Функция проверяет равенство 1 и последнего элемента, затем 2 и предпоследнего
+// если они равны возвращает true
+bool ItsTrue(int[] coll)
+{
+    bool ItsTrue = true;
+    for (int i = 0, j = coll.Length - 1; i < coll.Length; i++, j--)
+    {
+        ItsTrue = coll[i] == coll[j] ? true : false;
+    }
+    return ItsTrue;
+}
+
+// Итоговая функция, которая запускает все вышестоящие
+String FinalProcessing(int number)
+{
+    bool x = ItsTrue(GenerateArrFromNum(number));
+    return x ? $"{number} - палиндромом" : $"{number} - не палиндромом";
+}
+
+
+Console.Write("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FinalProcessing(num));
---
+++ homework/homework_3/S_3_Hw_Task_2/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Функция принимает пустой массив размера 3
+// заполняет его значениями x, y, z
+int[] EnterCoordinates(int[] EmptyArr)
+{
+    String[] NameCoord = { "x", "y", "z" };
+    for (int i = 0; i < NameCoord.Length; i++)
+    {
+        Console.Write($"{NameCoord[i]}: ");
+        EmptyArr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return EmptyArr;
+}
+
+// Функция принимает 2 массва размера 3
+// Высчитывает дистанцию между точками
+double СalculationDistance(int[] arr1, int[] arr2)
+{
+    double distance = Math.Round(Math.Sqrt(
+        Math.Pow(arr2[0] - arr1[0], 2) +
+        Math.Pow(arr2[1] - arr1[1], 2) +
+        Math.Pow(arr2[2] - arr1[2], 2)), 2);
+
+    return distance;
+}
+
+// Итоговая функция, которая запускает все вышестоящие
+String FinalProcessing(int[] array1, int[] array2)
+{
+    Console.WriteLine("Введи координаты первой точки: ");
+    int[] arr1 = EnterCoordinates(array1);
+
+    Console.WriteLine("Введи координаты второй точки: ");
+    int[] arr2 = EnterCoordinates(array2);
+
+    double res = СalculationDistance(arr1, arr2);
+
+    return $"Расстояние {res}";
+}
+
+int[] PointA = new int[3];
+int[] PointB = new int[3];
+
+System.Console.WriteLine(FinalProcessing(PointA, PointB));
