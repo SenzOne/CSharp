7ee55ebef73015a4bdbb855c0702e046443ae209
Semen Sl <sem63sam@yandex.ru>
2023-02-06 13:38:03 +0400

    added tasks from the 4 workshop

---
+++ seminars/seminar_4/task_26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+System.Console.Write("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// int i = 1;
+// while (num >= 10)
+// {
+//     num = num / 10; // num/= 10;
+//     i++;
+// }
+// System.Console.WriteLine(i);
+
+int GetCountNumber(int InputNum)
+{
+    int i = 1;
+    while (InputNum >= 10)
+    {
+        InputNum = InputNum / 10; // num/= 10;
+        i++;
+    }
+    return i;
+}
+
+System.Console.WriteLine($"{num} -> {GetCountNumber(num)} ");
