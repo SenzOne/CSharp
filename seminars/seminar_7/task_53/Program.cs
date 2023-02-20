64b521ee55ca6bd27eb6799d4e751bfbd297730f
Semen Sl <sem63sam@yandex.ru>
2023-02-20 14:27:24 +0400

    add seminar 7

---
+++ seminars/seminar_7/task_53/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 53: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int Prompt(string massege)
+{
+    System.Console.Write(massege);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+
+int[,] CreaateArray(int m = 5, int n = 5)
+{
+    int[,] array = new int[m, n];
+    return array;
+}
+
+
+void PrintArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j],4}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int[,] FillArrayRandom(int[,] coll)
+{
+    for (int i = 0; i < coll.GetLength(0); i++)
+    {
+        for (int j = 0; j < coll.GetLength(1); j++)
+        {
+            coll[i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return coll;
+}
+
+
+int[,] ReplaceRowsFirstVsLast(int[,] coll)
+{
+    for (int j = 0; j < coll.GetLength(1); j++)
+    {
+        (coll[0, j], coll[coll.GetLength(1) - 1, j]) = ((coll[coll.GetLength(1) - 1, j]), coll[0, j]);
+    }
+    return coll;
+}
+
+void Main()
+{
+    int[,] arr = CreaateArray();
+    arr = FillArrayRandom(arr);
+    PrintArr(arr);
+    System.Console.WriteLine();
+    PrintArr(ReplaceRowsFirstVsLast(arr));
+}
+
+Main();
---
+++ seminars/seminar_7/task_55/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 55: Задайте двумерный массив. 
+// Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+int Prompt(string massege)
+{
+    System.Console.Write(massege);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+
+int[,] CreaateArray(int m = 5, int n = 9)
+{
+    int[,] array = new int[m, n];
+    return array;
+}
+
+
+void PrintArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j],4}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int[,] FillArrayRandom(int[,] coll)
+{
+    for (int i = 0; i < coll.GetLength(0); i++)
+    {
+        for (int j = 0; j < coll.GetLength(1); j++)
+        {
+            coll[i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return coll;
+}
+
+
+void ReplaceRowsFirstVsLast(int[,] coll)
+{
+    for (int i = 0; i < coll.GetLength(1); i++)
+    {
+        // System.Console.WriteLine(coll[i,0]);
+        for (int j = 0; j < coll.GetLength(0); j++)
+        {
+            System.Console.Write($"{coll[j,i],4}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Main()
+{
+    int[,] arr = CreaateArray();
+    arr = FillArrayRandom(arr);
+    PrintArr(arr);
+    System.Console.WriteLine();
+    ReplaceRowsFirstVsLast(arr);
+}
+
+Main();
