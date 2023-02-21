27f8ef68f4545b0556c232896bf2de72c8844bab
Semen Sl <sem63sam@yandex.ru>
2023-02-22 00:13:03 +0400

    upd task 62, add task 100

---
+++ homework/homework_8/task_100/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 62. Напишите программу, которая 
+// заполнит спирально через 1 строку массив n на n. 
+
+
+int Prompt(string massege)
+{
+    System.Console.Write(massege);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+string[,] CreaateArray(int m = 10, int n = 10)
+{
+    string[,] array = new string[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = " ";
+        }
+    }
+    return array;
+}
+
+
+void PrintArr(string[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j], 1}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+string[,] FillArray(string[,] arr, string sim = " ")
+{
+    int size = arr.GetLength(1);
+    string simbol = sim;
+
+    for (int delta = 0; delta < size - 2; delta += 2)
+    {
+        for (int i = 0 + delta; i < size - 1 - delta; i++)
+        {
+            arr[0 + delta, i] = simbol;
+        }
+
+    for (int i = 0 + delta; i < size - 1 - delta; i++)
+        {
+            arr[i, (size - 1) - 1 - delta] = simbol;
+        }
+
+    for (int i = size - 1 - 1 - delta; i >= 0 + 1 + delta; i--)
+        {
+            arr[(size - 1 - 1) - delta, i] = simbol;
+        }
+    for (int i = size - 1 - 1 - delta; i >= 1 + 1 + delta; i--)
+        {
+            arr[i, 0 + 1 + delta] = simbol;
+        }
+    }
+    return arr;
+}
+
+
+void Main()
+{   
+    int n = Prompt("введи размер спирали: ");
+    string[,] arr = CreaateArray(n, n);
+    arr = FillArray(arr, "*");
+    PrintArr(arr);
+}
+
+Main();
---
+++ homework/homework_8/task_62/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4. 
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] CreaateArray(int m = 30, int n = 30)
+{
+    int[,] array = new int[m, n];
+    return array;
+}
+
+void PrintArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j],3}  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int[,] arr)
+{
+
+    int num = 1;
+    int size = arr.GetLength(1);
+
+
+    for (int delta = 0; delta < size - 2; delta++)
+    {
+        for (int i = 0 + delta; i < size - delta; i++)
+        {
+            arr[0 + delta, i] = num;
+            num++;
+        }
+        num--;
+
+        for (int i = 0 + delta; i < size - delta; i++)
+        {
+            arr[i, (size - 1) - delta] = num;
+            num++;
+        }
+
+        num--;
+        for (int i = size - 1 - delta; i >= 0 + delta; i--)
+        {
+            arr[(size - 1) - delta, i] = num;
+            num++;
+        }
+        num--;
+        for (int i = size - 1 - delta; i >= 1 + delta; i--)
+        {
+            arr[i, 0 + delta] = num;
+            num++;
+        }
+    }
+    return arr;
+}
+
+
+void Main()
+{
+    int[,] arr = CreaateArray();
+    arr = FillArray(arr);
+    PrintArr(arr);
+}
+
+Main();
