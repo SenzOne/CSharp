9707f8b6b7242d3859a9f9987c1ee3edb1e36bcd
Semen Sl <sem63sam@yandex.ru>
2023-02-24 01:24:35 +0400

    add homework 9

---
+++ homework/homework_9/task_66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Prompt(string massege)
+{
+    System.Console.Write(massege);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+
+int GetSum(int min, int max)
+{
+    if (min == max) return max;
+    else return GetSum(min + 1, max) + min;
+}
+
+
+int n = Prompt("Введи первое число: ");
+int m = Prompt("Введи последнее число: ");
+
+System.Console.WriteLine(GetSum(n, m));
